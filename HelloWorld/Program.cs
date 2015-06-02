39664a0e3bb192efde0e61d91c5329d33abf43eb
cpilkington78 <pearldrums78@gmail.com>
2015-06-01 22:03:18 -0500

    My first commit.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "HELLO WORLD!";
+            string mySecondString = "Hope you have a nice day!";
+
+            Console.WriteLine("Your message is {0} to all who are out there.", myString);
+            Console.WriteLine(mySecondString);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
